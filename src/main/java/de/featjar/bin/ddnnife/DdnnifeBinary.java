2f487bf81bbc8961eee93bd789af0ad572929f20
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-02-23 16:50:59 +0100

    Updates licence header

--- src/main/java/de/featjar/bin/ddnnife/DdnnifeBinary.java
+++ src/main/java/de/featjar/bin/ddnnife/DdnnifeBinary.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2023 Sebastian Krieter, Elias Kuiter
  *
- * This file is part of FeatJAR-bin-sharpsat.
+ * This file is part of bin-ddnnife.
  *
- * bin-sharpsat is free software: you can redistribute it and/or modify it
+ * bin-ddnnife is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * bin-sharpsat is distributed in the hope that it will be useful,
+ * bin-ddnnife is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with bin-sharpsat. If not, see <https://www.gnu.org/licenses/>.
+ * along with bin-ddnnife. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatureIDE/FeatJAR-bin-sharpsat> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-bin-ddnnife> for further information.
  */
 package de.featjar.bin.ddnnife;
 
